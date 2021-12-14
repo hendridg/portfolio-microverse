4ccab0504f9bb1df6fd4addd4772ff24fc3b9c87
hendridg <hendridg@gmail.com>
2021-12-14 17:53:23 -0400

    Add method to show and close popup

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const btnCloseNav = document.querySelector('.btn-close-nav');
 const linkPortfolio = document.querySelector('.link-portfolio');
 const linkAbout = document.querySelector('.link-about');
 const linkContact = document.querySelector('.link-contact');
+let btnCloseDetailsMobile;
+const arrayBtnPopup = document.querySelectorAll('.btn-popup');
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.add('hidden');
@@ -28,4 +30,51 @@ linkAbout.addEventListener('click', () => {
 linkContact.addEventListener('click', () => {
   headerNavMobile.classList.add('hidden');
   menuBtn.classList.remove('hidden');
+});
+
+const cardWorkDetailsMobile = document.createElement('div');
+cardWorkDetailsMobile.innerHTML = `
+<article class="card-work-details-mobile">
+        <button type="button" class="btn-close-details">
+            <img src="./assest/Icon-Cancel.svg" alt="cancel icon">
+        </button>
+        <div class="card-image-details-mobile">
+            <img src="./assest/img/about-me.jpg" alt="a guy with a backpack in mountain">
+        </div>
+        <h2 class="card-title-mobile">
+            Keeping track of hundreds of
+            components
+        </h2>
+        <ul class="card-tags-mobile">
+            <li>Rubi on rails</li>
+            <li>css</li>
+            <li>JavaScript</li>
+            <li>html</li>
+        </ul>
+        <p class="card-paragraph-mobile">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it 1960s with the relea
+        </p>
+        <div class="card-button-mobile">
+            <button type="button" class="btn-card-details-mobile">
+                See Live
+                <img src="./assest/see_live_icon.svg" alt="see live icon">
+            </button>
+            <button type="button" class="btn-card-details-mobile">
+                See Sources
+                <img src="./assest/github_white.svg" alt="see github icon">
+            </button>
+        </div>
+    </article>
+`;
+
+arrayBtnPopup[5].addEventListener('click', async () => {
+  document.body.appendChild(cardWorkDetailsMobile);
+  btnCloseDetailsMobile = await document.querySelector('.btn-close-details');
+  btnCloseDetailsMobile.addEventListener('click', () => {
+    cardWorkDetailsMobile.remove();
+  });
 });
